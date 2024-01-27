8e712015e2c74e1c478c59920c4b2346704db9fe
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-01-27 21:40:20 +0200

    Added more tests

--- src/game.py
+++ src/game.py
@@ -7,66 +7,68 @@ class Game:
         self._new_grid()
         self.temporary_test_pos = (0,0)
         
-    def _start(self):
+    def start(self, command = None):
         game_printer.print_start()
-        self._add_game_tile_only_2s()
-        self._run_game()
+        self.add_game_tile_only_2s()
+        self._run_game(command)
 
     def _new_grid(self):
         for i in range(4):
             self._game_grid.append([0]*4)
-        
+
     def grid(self):
         return self._game_grid
-        
-    def _add_game_tile_only_2s(self):
+
+    def add_game_tile_only_2s(self):
         row = randint(0, 3)
         column = randint(0, 3)
- 
+
         while self._game_grid[row][column] != 0:
             row = randint(0, 3)
             column = randint(0, 3)
-        
+
         self.temporary_test_pos = (row, column)
         self._game_grid[row][column] = 2
 
-    def _run_game(self):
+    def _run_game(self, command = None):
         while True:
             game_printer.print_grid(self._game_grid)
-            command = input("Select command: ")
+            if not command:
+                command = input("Select command: ")
+                
             if command == "w":
-                self._move_up()
+                self.move_up()
             elif command == "s":
-                self._move_down()
+                self.move_down()
             elif command == "a":
-                self._move_left()
+                self.move_left()
             elif command == "d":
-                self._move_right()
+                self.move_right()
             elif command == "q":
                 break
-            
-    def _move_up(self):
+
+    def move_up(self):
         row = self.temporary_test_pos[0]
         column = self.temporary_test_pos[1]
         self._game_grid[row][column] = 0
         self._game_grid[0][column] = 2
         self.temporary_test_pos = (0, column)
 
-    def _move_down(self):
+    def move_down(self):
         row = self.temporary_test_pos[0]
         column = self.temporary_test_pos[1]
         self._game_grid[row][column] = 0
         self._game_grid[3][column] = 2
         self.temporary_test_pos = (3, column)
-        
-    def _move_left(self):
+
+    def move_left(self):
         row = self.temporary_test_pos[0]
         column = self.temporary_test_pos[1]
         self._game_grid[row][column] = 0
         self._game_grid[row][0] = 2
         self.temporary_test_pos = (row, 0)
-        
-    def _move_right(self):
+
+    def move_right(self):
         row = self.temporary_test_pos[0]
         column = self.temporary_test_pos[1]
         self._game_grid[row][column] = 0
--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -4,8 +4,50 @@ from game import Game
 class TestGame(unittest.TestCase):
     def setUp(self):
         self.game = Game()
-        
-    def test_create_grid(self):
+
+    def test_create_grid_blank(self):
         grid = self.game.grid()
         
         self.assertEqual(grid,[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+
+    def test_starting_grid(self):
+        self.game.start('q')
+        start_grid = self.game.grid()
+        
+        self.assertEqual(sum(map(sum, start_grid)),2)
+
+    def test_move_up(self):
+        self.game.add_game_tile_only_2s()
+        self.game.move_up()
+        grid = self.game.grid()
+        
+        self.assertEqual(sum(grid[0]), 2)
+
+    def test_move_down(self):
+        self.game.add_game_tile_only_2s()
+        self.game.move_down()
+        grid = self.game.grid()
+        
+        self.assertEqual(sum(grid[3]), 2)
+
+    def test_move_left(self):
+        self.game.add_game_tile_only_2s()
+        self.game.move_left()
+        grid = self.game.grid()
+        
+        left_column_sum = 0
+        for row in grid:
+            left_column_sum += row[0]
+            
+        self.assertEqual(left_column_sum, 2)
+
+    def test_move_right(self):
+        self.game.add_game_tile_only_2s()
+        self.game.move_right()
+        grid = self.game.grid()
+        
+        right_column_sum = 0
+        for row in grid:
+            right_column_sum += row[3]
+            
+        self.assertEqual(right_column_sum, 2)
