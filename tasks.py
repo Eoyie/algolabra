5b755049feccc5449fe28c4c72a5aac0041b037f
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-01-27 18:19:15 +0200

    Added more tasks

--- tasks.py
+++ tasks.py
@@ -7,3 +7,15 @@ def start(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
+    
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
