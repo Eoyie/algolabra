aee3e8bacbb08fcc31510b23bebc557bdb0fd96a
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-02-03 22:24:02 +0200

    Fix tests

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -1,33 +1,36 @@
 import unittest
 from game_manager import GameManager
+from play_game import PlayGame
 
 class TestGameManager(unittest.TestCase):
     ''' Temporary tests for current game code '''
     def setUp(self):
-        self.game = GameManager()
+        self.game = PlayGame()
+        self.game_manager = self.game.game_manager
 
-    def test_start(self):
-        score = self.game.score
+    def test_start_score(self):
+        self.game.start("q")
+        score = self.game.game_score()
 
         self.assertEqual(score, 0)
 
     def test_move_up(self):
-        self.game.move("UP")
-        grid = self.game.grid()
+        self.game_manager.move("UP")
+        grid = self.game_manager.grid()
         possible_sums = [4, 6]
 
         self.assertIn(sum(grid[0]), possible_sums)
 
     def test_move_down(self):
-        self.game.move("DOWN")
-        grid = self.game.grid()
+        self.game_manager.move("DOWN")
+        grid = self.game_manager.grid()
         possible_sums = [4, 6]
 
         self.assertIn(sum(grid[3]), possible_sums)
 
     def test_move_left(self):
-        self.game.move("LEFT")
-        grid = self.game.grid()
+        self.game_manager.move("LEFT")
+        grid = self.game_manager.grid()
         possible_sums = [4, 6]
 
         left_column_sum = 0
@@ -37,8 +40,8 @@ class TestGameManager(unittest.TestCase):
         self.assertIn(left_column_sum, possible_sums)
 
     def test_move_right(self):
-        self.game.move("RIGHT")
-        grid = self.game.grid()
+        self.game_manager.move("RIGHT")
+        grid = self.game_manager.grid()
         possible_sums = [4, 6]
 
         right_column_sum = 0
