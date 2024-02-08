14182a54cda311076597fc58e0375231b80696d6
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-02-08 23:07:55 +0200

    Made base for pygame ui

--- src/play_game.py
+++ src/play_game.py
@@ -1,3 +1,4 @@
+import pygame
 import game_printer
 from game_manager import GameManager
 from game_ai import GameExpectiminimax
@@ -9,6 +10,11 @@ class PlayGame:
         self.ai_enabled = False
         self.stage = True
         self.score = 0
+        
+        pygame.init()
+        self.screen = pygame.display.set_mode((500, 500))
+        self.font = pygame.font.SysFont(None, 48)
+        pygame.display.set_caption("2048")
 
     def start(self, command = None):
         ''' Starts up the game
@@ -23,41 +29,54 @@ class PlayGame:
         
             Temporary:
                 If not command check for a test. '''
-        while self.stage:
-            game_printer.print_grid(self.game_manager.grid())
-            if self.ai_enabled:
-                direction = self.game_ai.next_move(2)
-                self.game_manager.move(direction)
-            else:
-                self.take_input(command)
+        timer = pygame.time.Clock()
+        while True:
+            for event in pygame.event.get():
+                self.take_input(event)
+            #if self.ai_enabled:
+            #    direction = self.game_ai.next_move(2)
+            #    self.game_manager.move(direction)
+                
+            self.draw_board()
+            pygame.display.flip()
+            timer.tick(60)
 
 
-    def take_input(self, command):
+    def take_input(self, event):
         ''' Takes the inputs
         
             Temporary:
                 Will be changed with new ui.'''
-        if not command:
-            command = input("Select command: ")
-
-        if command == "w":
-            self.game_manager.move("UP")
-            command = None
-        elif command == "s":
-            self.game_manager.move("DOWN")
-            command = None
-        elif command == "a":
-            self.game_manager.move("LEFT")
-            command = None
-        elif command == "d":
-            self.game_manager.move("RIGHT")
-            command = None
-        elif command == "q":
-            self.stage = False
-        elif command == "enable":
-            self.ai_enabled = True
+                
+        if event.type == pygame.QUIT:
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.game_manager.move("UP")
+            elif event.key == pygame.K_DOWN:
+                self.game_manager.move("DOWN")
+            elif event.key == pygame.K_LEFT:
+                self.game_manager.move("LEFT")
+            elif event.key == pygame.K_RIGHT:
+                self.game_manager.move("RIGHT")
 
     def game_score(self):
         ''' Temporary for tests, will involve printing in the future.'''
         self.score = self.game_manager.score
         return self.score
+
+    def draw_board(self):
+        self.screen.fill((0,0,0))
+        game_grid = self.game_manager.grid()
+        x = 0
+        for i in range(4):
+            y = 0
+            for j in range(4):
+                rect = pygame.Rect(x, y, 125, 125)
+                if game_grid[j][i] != 0:
+                    number_text = str(game_grid[j][i])
+                    number = self.font.render(number_text, True, (200, 200, 200))
+                    self.screen.blit(number,(x+50,y+50))
+                pygame.draw.rect(self.screen, (200, 200, 200), rect, 1)
+                y += 125
+            x += 125
