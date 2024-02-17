17af8c7e8f076c1f5cc40b90ca385b8fcbc20c96
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-02-17 05:20:09 +0200

    Added two AI tests

--- src/tests/ai_test.py
+++ src/tests/ai_test.py
@@ -16,18 +16,36 @@ class TestGameManager(unittest.TestCase):
         self.assertAlmostEqual(time_reach_100, 10)
     """
 
-    def test_reach_10(self):
-        self.game.game_loop(10)
+    """def test_ai_runs(self):
+        self.game.game_loop(3)
         score = self.game.get_game_score()
 
-        self.assertGreaterEqual(score, 10)
+        self.assertGreaterEqual(score, 2)
+    """
 
-    def test_reach_2048(self):
-        grid = [[2, 2, 2, 2],
-                [2, 2, 2, 2],
-                [2, 2, 2, 2],
-                [2, 2, 2, 2]]
+    def test_ai_reach_2048(self):
+        grid = [[128, 128, 128, 128],
+                [128, 128, 128, 128],
+                [128, 128, 128, 128],
+                [128, 128, 128, 128]]
         self.assertTrue(self.game.set_grid(grid))
+
+        game_state = self.game.game_loop(6)
+        score = self.game.get_game_score()
+
+        self.assertGreaterEqual(score, 4096)
+        self.assertTrue(game_state)
+
+    def test_ai_lose(self):
+        grid = [[2, 128, 2, 128],
+                [128, 2, 128, 2],
+                [2, 128, 2, 128],
+                [128, 2, 128, 2]]
+        self.assertTrue(self.game.set_grid(grid))
+
+        game_state = self.game.game_loop(2)
+        self.assertFalse(game_state)
+
 class FakePlayGame:
     def __init__(self):
         self.game_manager = GameManager()
@@ -35,14 +53,16 @@ class FakePlayGame:
 
     def game_loop(self, mid_check: int):
         while True:
-            if self.game_manager.score() >= mid_check:
-                break
             if self.game_manager.check_game_end():
-                print("GAME LOST")
-                print(f"TOTAL SCORE: {self.game_manager.score()}")
+                return False
+            if mid_check == 0:
                 break
+
             direction = self.game_ai.next_move(2)
             self.game_manager.move(direction)
+            mid_check -= 1
+
+        return True
 
     def get_game_score(self):
         return self.game_manager.score()
