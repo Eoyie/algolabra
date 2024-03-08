2c4f3a4c6d6d07d0f095872a251553899daa1736
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-03-08 19:23:14 +0200

    Deleted unfinished minimax

--- src/game_ai.py
+++ src/game_ai.py
@@ -8,9 +8,6 @@ class GameExpectiminimax:
                         [2**5, 2**6, 2**7, 2**8],
                         [2**12, 2**11, 2**10, 2**9],
                         [2**13, 2**14, 2**15, 2**16]]
-        self.node_scores = 0
-        self.child_list = 0
-        self.node_number = 0
 
     def heuristic_evaluation(self, game_copy):
         ''' Heuristic evaluation of the game grid
@@ -67,11 +64,11 @@ class GameExpectiminimax:
             return self.heuristic_evaluation(game_copy), direction
 
         total_score = 0
-        # Player
+        # Player: Max
         if depth != int(depth):
             total_score = float('-inf')
             for direction_2 in self.directions:
-                game_copy_2 = copy.deepcopy(game_copy)
+                game_copy_2 = copy.deepcopy(game_copy) # Copy of the previous copy to test more moves
                 _ , moved = game_copy_2.move(direction_2, False)
                 if moved:
                     result = self.expectiminimax(game_copy_2, depth - 0.5,
@@ -79,7 +76,7 @@ class GameExpectiminimax:
                     if result > total_score:
                         total_score = result
 
-        # Computer
+        # Computer: Chance
         elif depth == int(depth):
             total_score = 0
             free_tiles = game_copy.get_free_tiles()
@@ -98,57 +95,3 @@ class GameExpectiminimax:
                 game_copy.add_game_tile(free_tile, 0)
 
         return (total_score, direction)
-
-
-    # Temporarely hidden because of pylint!
-    def get_minimax_move(self):
-        ''' Temporary start of minimax implementation 
-        self.node_scores = [0 for _ in range(50000)]
-        self.child_list = [[] for _ in range (50000)]
-        self.node_number = 1
-
-        #minimax
-
-        searched_value = self.node_scores[1]
-
-        for index, i in enumerate(self.child_list[1]):
-            if self.node_scores[i] == searched_value:
-                return self.directions[index]'''
-
-
-    def minimax(self, depth, node, alpha, beta):
-        ''' Temporary start of minimax implementation + alphabeta pruning
-        if depth == 0:
-            self.node_scores[node] = self.minimax_heuristic()
-            return self.node_scores[node]
-
-        # Player
-        if depth != int(depth):
-            for direction in self.directions:
-                game_copy = copy.deepcopy(self.game_manager)
-                self.node_number += 1
-                self.child_list[node].append(self.node_number)
-                _, moved = game_copy.move(direction, False)
-                if moved:
-                    alpha = max(alpha, self.minimax(depth - 0.5,
-                                                    self.node_number,
-                                                    alpha, beta))
-                if alpha >= beta:
-                    break
-            self.node_scores[node] = alpha
-            return alpha
-
-        # Computer
-        elif depth == int(depth):
-            possible_grids = []
-            grid_scores = []
-            free_tiles = self.game_manager.get_free_tiles()
-            game_copy = copy.deepcopy(self.game_manager)
-
-            for free_tile in free_tiles:
-                game_copy.add_game_tile(free_tile, 2)
-                possible_grids.append()
-                '''
-
-    def minimax_heuristic(self):
-        pass
